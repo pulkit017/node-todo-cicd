edc7b0d95f6d391f2b0f345e1eab3d6925a3cb86
Pulkit Jain <pulkit.jain8004@thepsi.com>
2023-06-07 17:25:37 +0530

    test

--- githooks/precommit.sh
+++ githooks/precommit.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sonar-scanner.bat -D"sonar.projectKey=node-todo-cicd" -D"sonar.sources=." -D"sonar.host.url=http://localhost:9000" -D"sonar.token=sqp_b913c8dd208c560106529643c68c44e7936d2c68"
+sonar-scanner.bat -D"sonar.projectKey=node-todo-cicd" -D"sonar.sources=." -D"sonar.host.url=http://localhost:9000" -D"sonar.login=admin" -D"sonar.password=1234"
 RESULT=$?
 [ $RESULT -ne 0 ] && exit 1
 exit 0
